348c8004e03cd88560ec3c8502617c1f4e8f6c09
VoliraGm <zhmysh.v@yandex.ru>
2022-09-17 18:15:58 +0300

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const Home = () => {
+    return (
+        <Container>
+            home
+        </Container>
+    )
+}
+
+const Container = styled.div`
+height: 100vh;
+
+`
