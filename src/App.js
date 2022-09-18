2fd321b80c47453fb0c72f2b73b212a43e10b4a1
VoliraGm <zhmysh.v@yandex.ru>
2022-09-18 09:43:13 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import { Header } from './components/Header'
 import { Home } from './components/Home'
-import { Section } from './components/Section'
 
 export const App = () => {
   return (
     <div>
       <Header />
-      <Home />
-      <Section />
+      <Home /> 
     </div>
   )
 }
